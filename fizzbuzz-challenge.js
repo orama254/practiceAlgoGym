4e35629bee1e914ac3802562b507b70d7c749758
orama254 <orama209@gmail.com>
2019-10-17 14:26:00 +0300

    fizzbuzz-challenge

---
+++ fizzbuzz-challenge.js
@@ -0,0 +1,19 @@
+const fizzbuzz = (num) =>{
+
+    for(let i = 0; i <= num; i++) {
+        //check if the number is a multiple of 3
+        if(1 % 3 === 0) {
+            console.log('fizz');
+        } //check if the number is a multiple of 3
+        else if(i % 5 === 0) {
+            console.log('buzz');
+        } //check if the number is a multiple of 3 and 5
+        else if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizzbuzz');
+        }
+        else {
+            console.log(i);
+        }
+    } 
+    
+};

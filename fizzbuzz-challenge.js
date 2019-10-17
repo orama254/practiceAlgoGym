ebe366ae4b3a81a97e4525c3af927b373f86ee18
orama254 <orama209@gmail.com>
2019-10-17 14:36:37 +0300

    called the fizzbuzz function

--- fizzbuzz-challenge.js
+++ fizzbuzz-challenge.js
@@ -1,15 +1,15 @@
-const fizzbuzz = (num) =>{
+const fizzbuzz = (num) => {
 
-    for(let i = 0; i <= num; i++) {
+    for(let i = 0; i <= 100; i++) {
         //check if the number is a multiple of 3
         if(1 % 3 === 0) {
-            console.log('fizz');
+            console.log("fizz");
         } //check if the number is a multiple of 3
         else if(i % 5 === 0) {
-            console.log('buzz');
+            console.log("buzz");
         } //check if the number is a multiple of 3 and 5
         else if(i % 3 === 0 && i % 5 === 0){
-            console.log('fizzbuzz');
+            console.log("fizzbuzz");
         }
         else {
             console.log(i);
@@ -17,3 +17,4 @@ const fizzbuzz = (num) =>{
     } 
     
 };
+fizzbuzz(100);

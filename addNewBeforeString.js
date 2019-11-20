7d8c97528db254058cff0bb2cbba4d7f92089d6a
orama254 <orama209@gmail.com>
2019-11-20 20:23:22 +0300

    Add New before every string

---
+++ addNewBeforeString.js
@@ -0,0 +1,11 @@
+/**
+ * Write a Javascript program to create a new string adding "New!" in front of a given string.
+ * If the given string begins with "New!" then return the original string
+ */
+
+
+ const addNew = (str) =>
+        str.indexOf('New!') === 0 ? str : `New! ${str}`;
+
+console.log(addNew('New! Offers'));        
+console.log(addNew('Offers'));

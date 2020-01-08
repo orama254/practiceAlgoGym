f5e567c19f94926fe1d86e07078957d138742b71
orama254 <orama209@gmail.com>
2020-01-08 19:59:51 +0300

    added practice Algo cardio session

---
+++ extractFirstHalf.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Write a Javascript program to extract the first half of a string of even length
+ */
+
+ const extFirstHalf = (str) => str.slice(0, str.length / 2);
+
+ console.log(extFirstHalf('Standard'));
+ console.log(extFirstHalf('investment'));
+ console.log(extFirstHalf('moppers'));
